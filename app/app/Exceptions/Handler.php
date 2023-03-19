cab3a352db35692d81025b721f113c8cbdb5c142
FelipeMenezesDM <contato@felipemenezes.com.br>
2023-03-18 23:07:25 -0300

    feat: configuração de infraestrutura da aplicação

--- app/app/Exceptions/Handler.php
+++ app/app/Exceptions/Handler.php
@@ -2,6 +2,11 @@
 
 namespace App\Exceptions;
 
+use FelipeMenezesDM\LaravelLoggerAdapter\LogHandler;
+use FelipeMenezesDM\LaravelLoggerAdapter\LogPayload;
+use FelipeMenezesDM\LaravelCommons\Enums\HttpStatusCode;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
 
@@ -41,8 +46,44 @@ class Handler extends ExceptionHandler
      */
     public function register(): void
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function(Throwable $throwable, Request $request) {
+            $status = HttpStatusCode::HTTP_INTERNAL_SERVER_ERROR;
+            $class = get_class($throwable);
+
+            if(property_exists($class, 'status')) {
+                $status = HttpStatusCode::tryFrom($throwable->status);
+            }elseif(method_exists($class, 'getStatusCode')) {
+                $status = HttpStatusCode::tryFrom($throwable->getStatusCode());
+            }elseif($throwable instanceof AuthenticationException) {
+                $status = HttpStatusCode::HTTP_UNAUTHORIZED;
+            }
+
+            $payload = [
+                'status'        => $status->value,
+                'error'         => $status->name,
+                'message'       => $status->getStatusMessage(),
+                'path'          => $request->path(),
+                'details'       => [],
+                'links'         => [
+                    'self' => $request->fullUrl(),
+                ],
+                'timestamp'     => now()->format('Y-m-d H:i:s.v'),
+            ];
+
+            if(method_exists($class, 'errors')) {
+                $payload['details'] = $throwable->errors();
+            }
+
+            LogHandler::error(null, LogPayload::build()
+                ->setEndPoint($payload['path'])
+                ->setHttpStatus($payload['status'])
+                ->setLogCode($request->get('logCode'))
+                ->setMessage($throwable->getMessage())
+                ->setDetails([
+                    'errors'    => $payload['details'],
+                    'trace'     => method_exists($class, 'getTrace') ? $throwable->getTrace() : []
+                ])
+                ->setCodeLine(basename($throwable->getFile()) . ": " . $throwable->getLine()));
         });
     }
 }
--- app/app/Providers/AppServiceProvider.php
+++ app/app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +22,40 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->enableSSL();
+        $this->appSetLocale();
+        $this->schemaSetDefaultStringLength();
+    }
+
+    /**
+     * Set global location for application
+     *
+     * @return void
+     */
+    private function appSetLocale() : void
+    {
+        App::setLocale(config('app.locale'));
+    }
+
+    /**
+     * Set default string length for schema
+     *
+     * @return void
+     */
+    private function schemaSetDefaultStringLength() : void
+    {
+        Schema::defaultStringLength(150);
+    }
+
+    /**
+     * Enable SSL
+     *
+     * @return void
+     */
+    private function enableSSL() : void
+    {
+        if(env('ENABLE_SSL', false)) {
+            URL::forceScheme('https');
+        }
     }
 }
