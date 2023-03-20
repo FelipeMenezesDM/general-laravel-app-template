8b6dae9113d89c336cd9d3280250575371ea7979
FelipeMenezesDM <contato@felipemenezes.com.br>
2023-03-20 09:32:00 -0300

    feat: configuração do pacote de testes integrados

---
+++ tests/src/Steps/Common.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Src\Steps;
+
+use Behat\Behat\Context\Context;
+
+class Common implements Context
+{
+    /**
+     * @Given que uma requisição foi realizada para o endpoint de health check da aplicação
+     */
+    public function que_uma_requisicao_foi_realizada_para_o_endpoint_de_health_check_da_aplicacao() : void
+    {
+
+    }
+
+    /**
+     * @Then confirmar retorno de status http 200
+     */
+    public function confirmar_retorno_de_status_http_200() : void
+    {
+
+    }
+}
